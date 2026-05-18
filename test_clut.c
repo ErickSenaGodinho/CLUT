a979710f7c3f149d1bb8f94e9f18a2250a7131c5
Erick Sena Godinho <ericksena621@gmail.com>
2026-05-18 18:41:25 -0300

    feat: add meta testing for ci/cd

---
+++ test_clut.c
@@ -0,0 +1,237 @@
+#include <stdio.h>
+
+static FILE *g_dev_null = NULL;
+
+#define CLUT_META_TESTING
+#define CLUT_STREAM_DEFAULT stdout
+#define CLUT_STREAM_FAIL g_dev_null
+
+#define CLUT_OUTPUT_COLOR
+#define CLUT_IMPLEMENTATION
+#include "clut.h"
+
+void validate_pass(bool assert_condition, const char *macro_expression, const char *func_name, const char *file, int line) {
+  if (assert_condition) {
+    fprintf(stderr, "%s:%d:%s:", file, line, func_name);
+    fprintf(stderr, CLUT_STR_FAIL ":False-positive FAILURE detected\n");
+    fprintf(stderr, "  -> Expression: %s\n\n", macro_expression);
+    Clut.failures++;
+  }
+}
+
+void validate_fail(bool assert_condition, const char *macro_expression, const char *func_name, const char *file, int line) {
+  if (!assert_condition) {
+    fprintf(stderr, "%s:%d:%s:", file, line, func_name);
+    fprintf(stderr, CLUT_STR_FAIL ":False-positive PASS detected\n");
+    fprintf(stderr, "  -> Expression: %s\n\n", macro_expression);
+    Clut.failures++;
+  }
+}
+
+#define VALIDATE_PASS(assertion_expr)                                                                                                                                                                                      \
+  do {                                                                                                                                                                                                                     \
+    Clut.total_tests++;                                                                                                                                                                                                    \
+    Clut.current_test_failed = false;                                                                                                                                                                                      \
+    assertion_expr;                                                                                                                                                                                                        \
+    validate_pass(Clut.current_test_failed, #assertion_expr, __func__, __FILE__, __LINE__);                                                                                                                                \
+  } while (0)
+
+#define VALIDATE_FAIL(assertion_expr)                                                                                                                                                                                      \
+  do {                                                                                                                                                                                                                     \
+    Clut.total_tests++;                                                                                                                                                                                                    \
+    Clut.current_test_failed = false;                                                                                                                                                                                      \
+    assertion_expr;                                                                                                                                                                                                        \
+    validate_fail(Clut.current_test_failed, #assertion_expr, __func__, __FILE__, __LINE__);                                                                                                                                \
+  } while (0)
+
+void test_suite_boolean_and_basic(void) {
+  int val = 1;
+  VALIDATE_PASS(TEST_ASSERT(val == 1));
+  VALIDATE_FAIL(TEST_ASSERT(val == 0));
+
+  VALIDATE_PASS(TEST_ASSERT_TRUE(true));
+  VALIDATE_FAIL(TEST_ASSERT_TRUE(false));
+
+  VALIDATE_PASS(TEST_ASSERT_FALSE(false));
+  VALIDATE_FAIL(TEST_ASSERT_FALSE(true));
+
+  VALIDATE_PASS(TEST_ASSERT_UNLESS(false));
+  VALIDATE_FAIL(TEST_ASSERT_UNLESS(true));
+}
+
+void test_suite_pointers_and_nullness(void) {
+  int dummy = 42;
+  int *ptr = &dummy;
+
+  VALIDATE_PASS(TEST_ASSERT_NULL(NULL));
+  VALIDATE_FAIL(TEST_ASSERT_NULL(ptr));
+
+  VALIDATE_PASS(TEST_ASSERT_NOT_NULL(ptr));
+  VALIDATE_FAIL(TEST_ASSERT_NOT_NULL(NULL));
+
+  VALIDATE_PASS(TEST_ASSERT_EQUAL_PTR(ptr, ptr));
+  VALIDATE_FAIL(TEST_ASSERT_EQUAL_PTR(ptr, NULL));
+
+  VALIDATE_PASS(TEST_ASSERT_NOT_EQUAL_PTR(ptr, NULL));
+  VALIDATE_FAIL(TEST_ASSERT_NOT_EQUAL_PTR(ptr, ptr));
+}
+
+void test_suite_integer_comparisons(void) {
+  VALIDATE_PASS(TEST_ASSERT_EQUAL_INT(10, 10));
+  VALIDATE_FAIL(TEST_ASSERT_EQUAL_INT(10, 20));
+
+  VALIDATE_PASS(TEST_ASSERT_NOT_EQUAL_INT(10, 20));
+  VALIDATE_FAIL(TEST_ASSERT_NOT_EQUAL_INT(10, 10));
+
+  VALIDATE_PASS(TEST_ASSERT_GREATER_THAN_INT(5, 10));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_THAN_INT(10, 5));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_THAN_INT(10, 10));
+
+  VALIDATE_PASS(TEST_ASSERT_LESS_THAN_INT(10, 5));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_THAN_INT(5, 10));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_THAN_INT(5, 5));
+
+  VALIDATE_PASS(TEST_ASSERT_GREATER_OR_EQUAL_INT(5, 10));
+  VALIDATE_PASS(TEST_ASSERT_GREATER_OR_EQUAL_INT(10, 10));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_OR_EQUAL_INT(10, 5));
+
+  VALIDATE_PASS(TEST_ASSERT_LESS_OR_EQUAL_INT(10, 5));
+  VALIDATE_PASS(TEST_ASSERT_LESS_OR_EQUAL_INT(5, 5));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_OR_EQUAL_INT(5, 10));
+}
+
+void test_suite_floating_point_numbers(void) {
+  VALIDATE_PASS(TEST_ASSERT_EQUAL_FLOAT(3.14f, 3.14f));
+  VALIDATE_FAIL(TEST_ASSERT_EQUAL_FLOAT(3.14f, 2.71f));
+
+  VALIDATE_PASS(TEST_ASSERT_NOT_EQUAL_FLOAT(3.14f, 2.71f));
+  VALIDATE_FAIL(TEST_ASSERT_NOT_EQUAL_FLOAT(3.14f, 3.14f));
+
+  VALIDATE_PASS(TEST_ASSERT_GREATER_THAN_FLOAT(1.0f, 2.0f));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_THAN_FLOAT(2.0f, 1.0f));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_THAN_FLOAT(2.0f, 2.0f));
+
+  VALIDATE_PASS(TEST_ASSERT_GREATER_OR_EQUAL_FLOAT(1.0f, 2.0f));
+  VALIDATE_PASS(TEST_ASSERT_GREATER_OR_EQUAL_FLOAT(2.0f, 2.0f));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_OR_EQUAL_FLOAT(2.0f, 1.0f));
+
+  VALIDATE_PASS(TEST_ASSERT_LESS_THAN_FLOAT(2.0f, 1.0f));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_THAN_FLOAT(1.0f, 2.0f));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_THAN_FLOAT(1.0f, 1.0f));
+
+  VALIDATE_PASS(TEST_ASSERT_LESS_OR_EQUAL_FLOAT(2.0f, 1.0f));
+  VALIDATE_PASS(TEST_ASSERT_LESS_OR_EQUAL_FLOAT(1.0f, 1.0f));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_OR_EQUAL_FLOAT(1.0f, 2.0f));
+
+  VALIDATE_PASS(TEST_ASSERT_EQUAL_DOUBLE(3.14159, 3.14159));
+  VALIDATE_FAIL(TEST_ASSERT_EQUAL_DOUBLE(3.14159, 2.71828));
+
+  VALIDATE_PASS(TEST_ASSERT_NOT_EQUAL_DOUBLE(3.14159, 2.71828));
+  VALIDATE_FAIL(TEST_ASSERT_NOT_EQUAL_DOUBLE(3.14159, 3.14159));
+
+  VALIDATE_PASS(TEST_ASSERT_GREATER_THAN_DOUBLE(1.0, 2.0));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_THAN_DOUBLE(2.0, 1.0));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_THAN_DOUBLE(2.0, 2.0));
+
+  VALIDATE_PASS(TEST_ASSERT_GREATER_OR_EQUAL_DOUBLE(1.0, 2.0));
+  VALIDATE_PASS(TEST_ASSERT_GREATER_OR_EQUAL_DOUBLE(2.0, 2.0));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_OR_EQUAL_DOUBLE(2.0, 1.0));
+
+  VALIDATE_PASS(TEST_ASSERT_LESS_THAN_DOUBLE(2.0, 1.0));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_THAN_DOUBLE(1.0, 2.0));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_THAN_DOUBLE(1.0, 1.0));
+
+  VALIDATE_PASS(TEST_ASSERT_LESS_OR_EQUAL_DOUBLE(2.0, 1.0));
+  VALIDATE_PASS(TEST_ASSERT_LESS_OR_EQUAL_DOUBLE(1.0, 1.0));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_OR_EQUAL_DOUBLE(1.0, 2.0));
+}
+
+void test_suite_floating_point_precision_accumulation(void) {
+  float accumulated_float = 0.0f;
+  for (int i = 0; i < 10; i++) {
+    accumulated_float += 0.1f;
+  }
+  float expected_float = 1.0f;
+
+  double accumulated_double = 0.0;
+  for (int i = 0; i < 10; i++) {
+    accumulated_double += 0.1;
+  }
+  double expected_double = 1.0;
+
+  VALIDATE_PASS(TEST_ASSERT_EQUAL_FLOAT(expected_float, accumulated_float));
+  VALIDATE_FAIL(TEST_ASSERT_NOT_EQUAL_FLOAT(expected_float, accumulated_float));
+
+  VALIDATE_PASS(TEST_ASSERT_GREATER_THAN_FLOAT(accumulated_float, expected_float + 1.0f));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_THAN_FLOAT(expected_float + 1.0f, accumulated_float));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_THAN_FLOAT(accumulated_float, expected_float));
+
+  VALIDATE_PASS(TEST_ASSERT_GREATER_OR_EQUAL_FLOAT(accumulated_float, expected_float + 1.0f));
+  VALIDATE_PASS(TEST_ASSERT_GREATER_OR_EQUAL_FLOAT(accumulated_float, expected_float));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_OR_EQUAL_FLOAT(expected_float + 1.0f, accumulated_float));
+
+  VALIDATE_PASS(TEST_ASSERT_LESS_THAN_FLOAT(expected_float + 1.0f, accumulated_float));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_THAN_FLOAT(accumulated_float, expected_float + 1.0f));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_THAN_FLOAT(expected_float, accumulated_float));
+
+  VALIDATE_PASS(TEST_ASSERT_LESS_OR_EQUAL_FLOAT(expected_float + 1.0f, accumulated_float));
+  VALIDATE_PASS(TEST_ASSERT_LESS_OR_EQUAL_FLOAT(expected_float, accumulated_float));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_OR_EQUAL_FLOAT(accumulated_float, expected_float + 1.0f));
+
+  VALIDATE_PASS(TEST_ASSERT_EQUAL_DOUBLE(expected_double, accumulated_double));
+  VALIDATE_FAIL(TEST_ASSERT_NOT_EQUAL_DOUBLE(expected_double, accumulated_double));
+
+  VALIDATE_PASS(TEST_ASSERT_GREATER_THAN_DOUBLE(accumulated_double, expected_double + 1.0));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_THAN_DOUBLE(expected_double + 1.0, accumulated_double));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_THAN_DOUBLE(accumulated_double, expected_double));
+
+  VALIDATE_PASS(TEST_ASSERT_GREATER_OR_EQUAL_DOUBLE(accumulated_double, expected_double + 1.0));
+  VALIDATE_PASS(TEST_ASSERT_GREATER_OR_EQUAL_DOUBLE(accumulated_double, expected_double));
+  VALIDATE_FAIL(TEST_ASSERT_GREATER_OR_EQUAL_DOUBLE(expected_double + 1.0, accumulated_double));
+
+  VALIDATE_PASS(TEST_ASSERT_LESS_THAN_DOUBLE(expected_double + 1.0, accumulated_double));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_THAN_DOUBLE(accumulated_double, expected_double + 1.0));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_THAN_DOUBLE(expected_double, accumulated_double));
+
+  VALIDATE_PASS(TEST_ASSERT_LESS_OR_EQUAL_DOUBLE(expected_double + 1.0, accumulated_double));
+  VALIDATE_PASS(TEST_ASSERT_LESS_OR_EQUAL_DOUBLE(expected_double, accumulated_double));
+  VALIDATE_FAIL(TEST_ASSERT_LESS_OR_EQUAL_DOUBLE(accumulated_double, expected_double + 1.0));
+}
+
+void test_suite_strings(void) {
+  VALIDATE_PASS(TEST_ASSERT_EQUAL_STRING("clut", "clut"));
+  VALIDATE_FAIL(TEST_ASSERT_EQUAL_STRING("clut", "different"));
+  VALIDATE_FAIL(TEST_ASSERT_EQUAL_STRING("clut", NULL));
+
+  VALIDATE_PASS(TEST_ASSERT_NOT_EQUAL_STRING("clut", "different"));
+  VALIDATE_FAIL(TEST_ASSERT_NOT_EQUAL_STRING("clut", "clut"));
+
+  VALIDATE_PASS(TEST_ASSERT_EQUAL_STRING_LEN("clut_framework", "clut_testing", 5));
+  VALIDATE_FAIL(TEST_ASSERT_EQUAL_STRING_LEN("clut_framework", "clut_testing", 10));
+
+  VALIDATE_PASS(TEST_ASSERT_NOT_EQUAL_STRING_LEN("clut_framework", "clut_testing", 10));
+  VALIDATE_FAIL(TEST_ASSERT_NOT_EQUAL_STRING_LEN("clut_framework", "clut_testing", 5));
+}
+
+int main(void) {
+#ifdef _WIN32
+  g_dev_null = fopen("nul", "w");
+#else
+  g_dev_null = fopen("/dev/null", "w");
+#endif
+
+  TEST_BEGIN();
+
+  TEST_RUN(test_suite_boolean_and_basic);
+  TEST_RUN(test_suite_pointers_and_nullness);
+  TEST_RUN(test_suite_integer_comparisons);
+  TEST_RUN(test_suite_floating_point_numbers);
+  TEST_RUN(test_suite_floating_point_precision_accumulation);
+  TEST_RUN(test_suite_strings);
+
+  int exit_code = TEST_END();
+
+  if (g_dev_null)
+    fclose(g_dev_null);
+  return exit_code;
+}
