836f5108e8f118a0f893d8f194180d41d79a6914
Erick Sena Godinho <ericksena621@gmail.com>
2026-07-08 19:23:23 -0300

    feat: add parameterized test to anonymous_structs example

--- examples/anonymous_structs.c
+++ examples/anonymous_structs.c
@@ -6,6 +6,12 @@ typedef struct {
   float y;
 } Vec2;
 
+typedef struct {
+  Vec2 a;
+  Vec2 b;
+  float expected;
+} Vec2DotCase;
+
 Vec2 vec2_add(Vec2 a, Vec2 b) { return (Vec2){a.x + b.x, a.y + b.y}; }
 float vec2_dot(Vec2 a, Vec2 b) { return a.x * b.x + a.y * b.y; }
 
@@ -27,12 +33,24 @@ TEST(vec2_fields) {
   TEST_ASSERT_EQUAL_FLOAT(expected.y, result.y);
 }
 
+PARAM_TEST(vec2_dot_cases, Vec2DotCase,
+           {
+               {{2.0f, 3.0f}, {4.0f, 5.0f}, 23.0f},
+               {{1.0f, 0.0f}, {0.0f, 1.0f}, 0.0f},
+               {{1.0f, 0.0f}, {1.0f, 0.0f}, 1.0f},
+               {{-1.0f, -1.0f}, {1.0f, 1.0f}, -2.0f},
+           }) {
+  float result = vec2_dot(input.a, input.b);
+  TEST_ASSERT_EQUAL_FLOAT(input.expected, result);
+}
+
 int main(void) {
   RUNNER_BEGIN();
   SUITE_BEGIN();
   RUN_TEST(vec2_add);
   RUN_TEST(vec2_dot);
   RUN_TEST(vec2_fields);
+  RUN_TEST(vec2_dot_cases);
   SUITE_END();
   return RUNNER_END();
 }
